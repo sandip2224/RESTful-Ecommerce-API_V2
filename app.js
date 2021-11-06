ea20e9028f8f8459266febaa60579ae4ca89e20f
sandip2224 <sandipan2224@gmail.com>
2021-11-06 20:33:33 +0530

    ✨ Added express rate limiter on all API endpoints

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const colors = require('colors')
 const helmet = require('helmet')
 const cors = require('cors')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
 require('dotenv').config({ path: './config/config.env' })
 
 // Initialize database
@@ -10,6 +11,11 @@ const db = require('./config/db')
 const itemModel = require('./api/models/Item')
 const orderModel = require('./api/models/Order')
 const userModel = require('./api/models/User')
+
+const limiter = rateLimit({
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    max: 150 // limit each IP to 100 requests per windowMs
+});
 
 // Each item belongs to one or more order, each order belongs to exactly one item
 itemModel.hasMany(orderModel)
@@ -29,6 +35,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
 app.use(helmet())
+app.use(limiter)
 
 app.use('/api/v1/items', require('./api/routes/itemRoute'))
 app.use('/api/v1/orders', require('./api/routes/orderRoute'))
